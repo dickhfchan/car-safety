398aec11d37f90466aceaf4461dbaf7016030d88
phphe <phphe@outlook.com>
2017-05-02 15:07:52 +0800

    Map:vue Alert information completed

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,45 +1,55 @@
 <template>
-  <div class="p-a">
-    <md-card>
-      <md-card-header>
-        <div class="md-title">Google Map<fullscreen-button></fullscreen-button></div>
-      </md-card-header>
+  <div>
+    <md-layout md-gutter>
+      <md-layout md-flex-medium="100" md-flex-large="50">
+        <md-card class="flex-1 m-a">
+          <md-card-header>
+            <div class="md-title">Google Map<fullscreen-button></fullscreen-button></div>
+          </md-card-header>
+          <md-card-content>
+            <GoogleMapTrackRender :ak="$store.state.googleMapAK" :points="points" style="width:100%;"></GoogleMapTrackRender>
+          </md-card-content>
+        </md-card>
+      </md-layout>
 
-      <md-card-content>
-        <GoogleMapTrackRender :ak="$store.state.googleMapAK" :points="points" style="width:100%;"></GoogleMapTrackRender>
-        <md-toolbar md-theme="green" style="color: #fff;min-height: inherit;padding: .5em 8px;">
-          <h1 class="md-title">Alert Information</h1>
-        </md-toolbar>
-        <md-table v-once>
-          <md-table-header>
-            <md-table-row>
-              <md-table-head>Warning</md-table-head>
-              <md-table-head>Start Time</md-table-head>
-              <md-table-head>End Time</md-table-head>
-              <md-table-head>Duration</md-table-head>
-              <md-table-head>Latitude</md-table-head>
-              <md-table-head>Longitude</md-table-head>
-              <md-table-head>Start Speed</md-table-head>
-              <md-table-head>End Speed</md-table-head>
-              <md-table-head>Top Speed</md-table-head>
-              <md-table-head>TI</md-table-head>
-              <md-table-head>HW</md-table-head>
-              <md-table-head>Near HW</md-table-head>
-              <md-table-head>SS</md-table-head>
-              <md-table-head>SX</md-table-head>
-            </md-table-row>
-          </md-table-header>
+      <md-layout  md-flex-medium="100" md-flex-large="50">
+        <md-card  class="flex-1 m-a">
+          <md-card-header>
+            <div class="md-title">Alert Information<fullscreen-button></fullscreen-button></div>
+          </md-card-header>
+          <md-card-content>
+            <md-table v-once>
+              <md-table-header>
+                <md-table-row>
+                  <md-table-head>Warning</md-table-head>
+                  <md-table-head>Start Time</md-table-head>
+                  <md-table-head>End Time</md-table-head>
+                  <md-table-head>Duration</md-table-head>
+                  <md-table-head>Latitude</md-table-head>
+                  <md-table-head>Longitude</md-table-head>
+                  <md-table-head>Start Speed</md-table-head>
+                  <md-table-head>End Speed</md-table-head>
+                  <md-table-head>Top Speed</md-table-head>
+                  <md-table-head>TI</md-table-head>
+                  <md-table-head>HW</md-table-head>
+                  <md-table-head>Near HW</md-table-head>
+                  <md-table-head>SS</md-table-head>
+                  <md-table-head>SX</md-table-head>
+                </md-table-row>
+              </md-table-header>
 
-          <md-table-body>
-            <md-table-row v-for="(row, index) in 11" :key="index">
-              <md-table-cell v-for="(col, index) in 14" :key="index">10</md-table-cell>
-            </md-table-row>
-          </md-table-body>
-        </md-table>
-      </md-card-content>
-    </md-card>
+              <md-table-body>
+                <md-table-row v-for="(row, index) in 11" :key="index">
+                  <md-table-cell v-for="(col, index) in 14" :key="index">10</md-table-cell>
+                </md-table-row>
+              </md-table-body>
+            </md-table>
+          </md-card-content>
+        </md-card>
+      </md-layout>
+    </md-layout>
 
-    <md-card class="m-t">
+    <md-card class="m-a">
       <md-card-header>
         <div class="md-title">Baidu Map<fullscreen-button></fullscreen-button></div>
       </md-card-header>
