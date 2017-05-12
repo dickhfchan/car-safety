13dcea515e405d670004041a773b074b8a50edee
dickhfchan <dickhfchan@timingpower.com>
2017-05-12 12:47:51 +0800

    Create config.py

---
+++ server/config.py
@@ -0,0 +1,10 @@
+
+##DATABASE CONNECTION PARAMETERS:
+db_username = "root"
+db_password = "password"
+db_name = "local"
+host = '127.0.0.1'
+port = 3306
+
+##GOOGLE API: CHANGE ONLY WHEN NECESSARY
+API_KEY = "AIzaSyCRJiQRpULDNnsylPwEgDu8XhgLN6kmu8I"
