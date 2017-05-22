1c04158efa5d5ff5ac78b6e5769532fd744e5187
dickhfchan <dickhfchan@timingpower.com>
2017-05-22 18:31:49 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,10 +1,13 @@
-
 ##DATABASE CONNECTION PARAMETERS:
 db_username = "root"
-db_password = "password"
-db_name = "local"
+db_password = "Knode$2017"
+db_name = "gds_frontend"
 host = '127.0.0.1'
 port = 3306
 
 ##GOOGLE API: CHANGE ONLY WHEN NECESSARY
 API_KEY = "AIzaSyCRJiQRpULDNnsylPwEgDu8XhgLN6kmu8I"
+
+#For Baidu
+baidu_apk = "0WbyzDGMdtHjqr2rW4EZ1HGrKb2vdbpG"
+baidu_service_id = '139574'
